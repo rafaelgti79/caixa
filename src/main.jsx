139b021e8727ce5839b66539388cefe634d35e0c
rafaelgti79 <rafaelgti79@gmail.com>
2025-06-10 11:09:02 -0300

    Rotas

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "cadastro",
+        path: "/cadastro",
         element: <Cadastro />,
       },
     ],
