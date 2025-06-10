64c07c50d6ae7c98a15b26a1be40695fd82b2697
rafaelgti79 <rafaelgti79@gmail.com>
2025-06-10 11:22:31 -0300

    Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import Home from "./pages/Home/home";
 
 function App() {
 
   return (
     <div>
    <Navbar/>
+   <Home/>
    <Outlet />
    
    </div>
